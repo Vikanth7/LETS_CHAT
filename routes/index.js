f2705e6b7528f57bf37c3ac22780e9048f596b25
Vikanth7 <21eg106b45@anurag.edu.in>
2023-09-27 18:42:44 +0530

    new

--- routes/index.js
+++ routes/index.js
@@ -2,11 +2,11 @@ var express = require('express');
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+router.get('/', ()=> {
   res.render('index');
 });
 
-router.get('/views/register',function(req, res, next) {
+router.get('/views/register',()=> {
   res.render('register');
 })
 
